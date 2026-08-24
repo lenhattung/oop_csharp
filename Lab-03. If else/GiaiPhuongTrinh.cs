58abdd95777cbaffc3bafaf412b190f737053405
Le Nhat Tung <lenhattung@gmail.com>
2026-08-24 18:41:41 +0700

    u
    u

---
+++ Lab-03. If else/GiaiPhuongTrinh.cs
@@ -0,0 +1,37 @@
+﻿public class GiaiPhuongTrinh
+{
+    public void GPTBacNhat()
+    {
+        // ax + b = 0;
+        double a, b, x;
+        Console.WriteLine("Nhap vao a, va b la he so cua pt: ax + b = 0");
+        Console.Write("a = ");
+        Double.TryParse(Console.ReadLine(), out a);
+        Console.Write("b = ");
+        Double.TryParse(Console.ReadLine(), out b);
+
+        if (a != 0)
+        {
+            x = -b / a;
+            Console.WriteLine($"Phuong trinh co nghiem x = {x}");
+        }
+        else
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Phuong trinh co vo so nghiem");
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+        }
+    }
+
+    public void GPTBacHai()
+    {
+        // ax^2 + bx + c = 0;
+
+    }
+}
+
---
+++ Lab-03. If else/Program.cs
@@ -0,0 +1,47 @@
+﻿// if (bieu thuc dieu kien) { cac cau lenh };
+// if (bieu thuc dieu kien) { cac cau lenh } else { cac cau lenh };
+Console.Write("Nhap vao bien x: ");
+int x;
+Int32.TryParse(Console.ReadLine(), out x); ;
+
+//if (x % 2 == 0)
+//{
+//    Console.WriteLine($"{x} la so chan");
+//}
+//else
+//{
+//    Console.WriteLine($"{x} la so le");
+//}
+
+// Toan tu dieu kien: (bieu thuc)? ket qua A : ket qua B
+
+/*
+string ketQua = (x % 2 == 0) ? "chan" : "le";
+Console.WriteLine($"{x} la {ketQua}");
+*/
+
+/*
+
+if (bieu thuc dieu kien 1) {
+    cac cau lenh khi bieu thuc dieu kien 1 dung
+}else if (btdk 2){
+    cac cau lenh khi bieu thuc dieu kien 2 dung
+} ... {
+    cac cau lenh khi bieu thuc dieu kien ... dung
+} else {
+    cac cau lenh khi tat cac cac bieu thuc ben tren bi sai
+}
+ 
+
+if (bieu thuc dieu kien){
+    if(btkd) {
+        if(btdk){
+            cac cau lenh
+        }
+    }
+}
+
+*/
+
+GiaiPhuongTrinh g = new GiaiPhuongTrinh();
+g.GPTBacNhat();
