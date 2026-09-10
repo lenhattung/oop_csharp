78ba5591553b411b14c17bc0b0834fd235476be8
Le Nhat Tung <lenhattung@gmail.com>
2026-09-10 19:15:47 +0700

    u
    u

---
+++ Lab 09. Static/PhepTinh.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab_09._Static
+{
+    public class PhepTinh
+    {
+        public static double PI = 3.14;
+        public static int BinhPhuong(int so) => so * so;
+
+        public static double TinhChuViHinhTron(double banKinh) => 2 * PI * banKinh;
+
+    }
+}
---
+++ Lab 09. Static/Program.cs
@@ -0,0 +1,16 @@
+﻿// static
+/*
+Thông thường, khi ta tạo một lớp và khai báo các biến, phương thức — những thành phần đó thuộc về đối tượng. Nghĩa là:
+Phải tạo đối tượng mới dùng được
+Mỗi đối tượng có bản sao riêng của biến đó → thay đổi ở một đối tượng không ảnh hưởng đến đối tượng khác
+Khi dùng từ khóa static → thành phần đó KHÔNG thuộc về đối tượng, mà thuộc về LỚP.
+ */
+// Gọi trực tiếp, KHÔNG cần new PhepToan()
+using Lab_09._Static;
+
+double kq = PhepTinh.BinhPhuong(5);
+Console.WriteLine($"5^2 = {kq}");
+
+
+double chuVi = PhepTinh.TinhChuViHinhTron(4);
+Console.WriteLine($"Chu vi = {chuVi}");
