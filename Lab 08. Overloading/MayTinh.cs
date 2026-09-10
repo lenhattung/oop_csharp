e44230110fc9c84b920d2d2043e222c64b6fb0f2
Le Nhat Tung <lenhattung@gmail.com>
2026-09-10 18:27:53 +0700

    u
    u

---
+++ Lab 08. Overloading/MayTinh.cs
@@ -0,0 +1,18 @@
+﻿
+public class MayTinh
+{
+    public int cong(int a, int b) => a + b;
+
+    //public int cong(int x, int y)
+    //{
+    //    return x + y;
+    //}
+
+    // Overloading
+    public double cong(double a, double b) => a + b;
+
+    public long cong(int x, long y) => x + y;
+    // Overloading
+    public int cong(int a, int b, int c) => a + b + c;
+}
+
---
+++ Lab 08. Overloading/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input a: ");
+int a = 0;
+Int32.TryParse(Console.ReadLine(), out a);
+Console.Write("Input b: ");
+int b = 0;
+Int32.TryParse(Console.ReadLine(), out b);
+
+MayTinh mt1 = new MayTinh();
+Console.WriteLine($"{a} + {b} = {mt1.cong(a, b)}");
+Console.WriteLine($"{a} + {b} + {a}= {mt1.cong(a, b, a)}");
+
+// Ctrl + Chuột trái
